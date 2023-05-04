bbdfad6b6695feeed6df5659bcf25bd69586953f
VendettaTVV <vvtkachukvv@gmail.com>
2023-05-04 21:22:46 +0300

    links & about info

---
+++ src/body/AbutUs.js
@@ -0,0 +1,26 @@
+import Container from "react-bootstrap/esm/Container";
+
+
+
+function InfoComponent() {
+    return (
+        <Container className='bg-light mt-0 mb-0'>
+
+            
+            <p className="text-start pt-5 fs-4 font-family-sans-serif mb-0 pb-5"> RKS OÜ (RKS LLC) is an industrial company that began its financial and economic activity in 2001. The main activity of the company comprises
+                bodywork and automotive painting. We work with private customers, legal persons as well as all insurance companies. We offer a wide range of services,
+                from small repairs (scratches on a car’s bodywork ) to repair of a damaged car. The following services are available at our workshop: auto glass replacement,
+                bodywork, paintwork, towing services, air conditioner filling, repair of plastic parts, custom auto parts , welding, restoration of body geometry, tyre fitting.
+                To establish the costs of repairs, vehicle inspection by a professional is required, if necessary, our specialist will come to the location of emergency transport.
+                The main advantages of the services provided by RKS OÜ are high-quality work, individual customer approach as well as production and modern technology improvement.
+                Our staff is actively involved in related training courses as repair technology is constantly changing. We only work with quality materials. Our body shop is
+                equipped with the most modern equipment: a new paint booth and a stand for restoration of body geometry, resistance welding, welding on aluminum, repair of
+                plastic parts, auto glass replacements. RKS OÜ is a member of AMTEL (International Organisation for Manufacturers of Motor Vehicles; www.amtel.ee), which allows
+                us to share experiences with similar foreign companies and international organisations. For the convenience of our customers, there is a large guarded parking on
+                the premises of the workshop.
+            </p>
+        </Container>
+    )
+}
+
+export default InfoComponent;
